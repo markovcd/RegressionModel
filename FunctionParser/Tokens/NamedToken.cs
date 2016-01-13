39d56ca530091e5e8c6a064c9688035905406c13
markovcd <markovcd@gmail.com>
2016-01-13 06:47:14 +0100

    Created Assert method

--- FunctionParser/Tokens/NamedToken.cs
+++ FunctionParser/Tokens/NamedToken.cs
@@ -4,9 +4,22 @@ namespace Markovcd.Classes
     public abstract class NamedToken : Token
     {
         public NamedToken(string name)
-            : base(name, $"\\b{name}\\b") { }
+            : base(name, $"\\b{name}\\b") 
+        {
+            Assert(name);
+        }
 
         public NamedToken(string name, int index)
-            : base(name, index, name.Length, name) { }
+            : base(name, index, name.Length, name) 
+        {
+            Assert(name);
+        }
+
+        private static void Assert(string name)
+        {
+            if (name == null) throw new ArgumentNullException(nameof(name));
+            if (!char.IsLetter(name[0])) throw new ArgumentException(nameof(name));
+            if (name.Skip(1).Any(c => (c != '_') && !char.IsLetterOrDigit(c))) throw new ArgumentException(nameof(name));
+        }
     }
 }
