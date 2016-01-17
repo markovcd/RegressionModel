479fc84511b5f661ded29a07cf32d252bf214f71
markovcd <markovcd@gmail.com>
2016-01-17 10:48:33 +0100

    Working generic versions of Model class.

--- RegressionModel/Model.cs
+++ RegressionModel/Model.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
@@ -6,46 +7,54 @@ using System.Linq.Expressions;
 using System.Text;
 
 namespace Markovcd.Classes
-{
-    public class Model<T, TResult> : Model<Func<T, TResult>>
-        where T : IConvertible
-        where TResult : IConvertible
+{//todo: typesafe calculatefunction
+    public sealed class Model<T, TResult> : Model<Func<T, TResult>>
+        where T : IComparable, IFormattable, IConvertible, IComparable<T>, IEquatable<T>
+        where TResult : IComparable, IFormattable, IConvertible, IComparable<TResult>, IEquatable<TResult>
     {
         public Model(Expression<Func<T, TResult>> func, IReadOnlyList<TResult> y, IReadOnlyList<T> x, IFormatProvider formatProvider = null)
-            : base(func, Convert(y), Convert(x)) { }
+            : base(func, Convert(y, formatProvider), Convert(x, formatProvider))
+        { }
     }
 
-    public class Model<T1, T2, TResult> : Model<Func<T1, T2, TResult>>
-        where T1 : IConvertible
-        where T2 : IConvertible
-        where TResult : IConvertible
+    public sealed class Model<T1, T2, TResult> : Model<Func<T1, T2, TResult>>
+        where T1 : IComparable, IFormattable, IConvertible, IComparable<T1>, IEquatable<T1>
+        where T2 : IComparable, IFormattable, IConvertible, IComparable<T2>, IEquatable<T2>
+        where TResult : IComparable, IFormattable, IConvertible, IComparable<TResult>, IEquatable<TResult>
     {
         public Model(Expression<Func<T1, T2, TResult>> func, IReadOnlyList<TResult> y, IReadOnlyList<T1> x1,
-            IReadOnlyList<T2> x2)
-            : base(func, Convert(y), Convert(x1), Convert(x2)) { }
+            IReadOnlyList<T2> x2, IFormatProvider formatProvider = null)
+            : base(func, Convert(y, formatProvider), Convert(x1, formatProvider), Convert(x2, formatProvider))
+        { }
     }
 
-    public class Model<T1, T2, T3, TResult> : Model<Func<T1, T2, T3, TResult>>
-        where T1 : IConvertible
-        where T2 : IConvertible
-        where T3 : IConvertible
-        where TResult : IConvertible
+    public sealed class Model<T1, T2, T3, TResult> : Model<Func<T1, T2, T3, TResult>>
+        where T1 : IComparable, IFormattable, IConvertible, IComparable<T1>, IEquatable<T1>
+        where T2 : IComparable, IFormattable, IConvertible, IComparable<T2>, IEquatable<T2>
+        where T3 : IComparable, IFormattable, IConvertible, IComparable<T3>, IEquatable<T3>
+        where TResult : IComparable, IFormattable, IConvertible, IComparable<TResult>, IEquatable<TResult>
     {
         public Model(Expression<Func<T1, T2, T3, TResult>> func, IReadOnlyList<TResult> y, IReadOnlyList<T1> x1,
-            IReadOnlyList<T2> x2, IReadOnlyList<T3> x3)
-            : base(func, Convert(y), Convert(x1), Convert(x2), Convert(x3)) { }
+            IReadOnlyList<T2> x2, IReadOnlyList<T3> x3, IFormatProvider formatProvider = null)
+            : base(
+                func, Convert(y, formatProvider), Convert(x1, formatProvider), Convert(x2, formatProvider),
+                Convert(x3, formatProvider))
+        { }
     }
 
-    public class Model<T1, T2, T3, T4, TResult> : Model<Func<T1, T2, T3, T4, TResult>>
-        where T1 : IConvertible
-        where T2 : IConvertible
-        where T3 : IConvertible
-        where T4 : IConvertible
-        where TResult : IConvertible
+    public sealed class Model<T1, T2, T3, T4, TResult> : Model<Func<T1, T2, T3, T4, TResult>>
+        where T1 : IComparable, IFormattable, IConvertible, IComparable<T1>, IEquatable<T1>
+        where T2 : IComparable, IFormattable, IConvertible, IComparable<T2>, IEquatable<T2>
+        where T3 : IComparable, IFormattable, IConvertible, IComparable<T3>, IEquatable<T3>
+        where T4 : IComparable, IFormattable, IConvertible, IComparable<T4>, IEquatable<T4>
+        where TResult : IComparable, IFormattable, IConvertible, IComparable<TResult>, IEquatable<TResult>
     {
         public Model(Expression<Func<T1, T2, T3, T4, TResult>> func, IReadOnlyList<TResult> y, IReadOnlyList<T1> x1,
-            IReadOnlyList<T2> x2, IReadOnlyList<T3> x3, IReadOnlyList<T4> x4)
-            : base(func, Convert(y), Convert(x1), Convert(x2), Convert(x3), Convert(x4)) { }
+            IReadOnlyList<T2> x2, IReadOnlyList<T3> x3, IReadOnlyList<T4> x4, IFormatProvider formatProvider = null)
+            : base(
+                func, Convert(y, formatProvider), Convert(x1, formatProvider), Convert(x2, formatProvider),
+                Convert(x3, formatProvider), Convert(x4, formatProvider))
+        { }
     }
 
     public class Model<TFunc> : Model where TFunc : class
@@ -56,23 +65,23 @@ namespace Markovcd.Classes
         public Model(Expression<TFunc> func, IReadOnlyList<double> y, params IReadOnlyList<double>[] x)
             : base(func, y, x) { }
 
-        protected static double Convert<T>(T value, IFormatProvider formatProvider = null)
+        private static double Convert<T>(T value, IFormatProvider formatProvider = null)
             where T : IConvertible
         {
             formatProvider = formatProvider ?? CultureInfo.InvariantCulture;
             return value.ToDouble(formatProvider);
         }
 
+        protected static IReadOnlyList<TResult> Convert<T, TResult>(IReadOnlyList<T> values, IFormatProvider formatProvider = null)
+            where T : IConvertible => values.Select(value => (TResult)System.Convert.ChangeType(value, typeof(TResult))).ToList();
+
         protected static IReadOnlyList<double> Convert<T>(IReadOnlyList<T> values, IFormatProvider formatProvider = null)
-            where T : IConvertible => values.Select(value => Convert(value, formatProvider)).ToList();
+            where T : IConvertible => Convert<T, double>(values, formatProvider);
     }
 
     public class Model : ModelBase
     {
-        protected readonly IReadOnlyList<LambdaExpression> SplitFunction;
-        protected readonly IReadOnlyList<LambdaExpression> SplitOriginalFunction;
-        protected readonly LambdaExpression ParamsFunction;
-        protected readonly LambdaExpression OutputParamsFunction;
+        private readonly IReadOnlyList<LambdaExpression> splitFunction;
         
         public LambdaExpression InputFunction { get; }
         public LambdaExpression OutputFunction { get; }      
@@ -81,7 +90,7 @@ namespace Markovcd.Classes
         public IReadOnlyList<double> Y { get; }
         public IReadOnlyList<double> Coefficients { get; }
         public double RSquared { get; }
-        public int ParameterCount { get; }
+        public int ParameterCount => X.Count;
 
         public Model(LambdaExpression func, IReadOnlyList<double> y, params IReadOnlyList<double>[] x)
         {
@@ -89,22 +98,18 @@ namespace Markovcd.Classes
 
             X = x;
             Y = y;
-            ParameterCount = x.Length;
-            
-            ParamsFunction = func.ToParams<double>();
-            SplitFunction = SplitAndCheckLambda(ParamsFunction).ToList();
-            Coefficients = GetCoefficients(SplitFunction, y, x);
-            
-            OutputParamsFunction = JoinFunction(SplitFunction, Coefficients);
-            RSquared = CalculateRSquared(OutputParamsFunction.Compile(), y, x);
-
             InputFunction = func;
-            SplitOriginalFunction = SplitAndCheckLambda(InputFunction).ToList();
-            OutputFunction = JoinFunction(SplitOriginalFunction, Coefficients);
+
+            splitFunction = SplitAndCheckLambda(InputFunction).ToList();
+            Coefficients = GetCoefficients(splitFunction, y, x);
+            
+            splitFunction = SplitAndCheckLambda(InputFunction).ToList();
+            OutputFunction = JoinFunction(splitFunction, Coefficients);
+            RSquared = CalculateRSquared(OutputFunction.Compile(), y, x);
         }
 
-        public double Calculate(params double[] x) 
-            => Calculate(OutputParamsFunction, x);
+        /*public double Calculate(params double[] x) 
+            => Calculate(OutputParamsFunction, x);*/
 
         private static string TrimParentheses(string func)
         {
@@ -119,10 +124,10 @@ namespace Markovcd.Classes
         {
             var parameters = InputFunction.Parameters.Select(p => p.Name).Aggregate((s1, s2) => $"{s1}, {s2}");
 
-            var terms = SplitOriginalFunction.Select(f => f.Body.ToString())
-                                             .Select(TrimParentheses)
-                                             .Select(s => new string(s.Where(c => !char.IsWhiteSpace(c)).ToArray()))
-                                             .ToArray();
+            var terms = splitFunction.Select(f => f.Body.ToString())
+                                     .Select(TrimParentheses)
+                                     .Select(s => new string(s.Where(c => !char.IsWhiteSpace(c)).ToArray()))
+                                     .ToArray();
 
 
             var coeffs = Coefficients.Select((d, i) => $"b{i + 1} = {d}").Aggregate((s1, s2) => $"{s1}\n{s2}");
@@ -152,7 +157,7 @@ namespace Markovcd.Classes
 
         private static void Assert(Type type)
         {
-            var doubl = typeof(double);
+            //var doubl = typeof(double);
 
             if (!type.IsSubclassOf(typeof(Delegate)) || !type.Name.Contains("Func`"))
                 throw new InvalidOperationException(type.Name + " is not a Func<> type");
@@ -162,36 +167,37 @@ namespace Markovcd.Classes
         }
 
         public static Model<T, TResult> Create<T, TResult>(Expression<Func<T, TResult>> func,
-            IReadOnlyList<TResult> y, IReadOnlyList<T> x)
-            where T : IConvertible
-            where TResult : IConvertible
-            => new Model<T, TResult>(func, y, x);
+            IReadOnlyList<TResult> y, IReadOnlyList<T> x, IFormatProvider formatProvider = null)
+            where T : IComparable, IFormattable, IConvertible, IComparable<T>, IEquatable<T>
+            where TResult : IComparable, IFormattable, IConvertible, IComparable<TResult>, IEquatable<TResult>
+            => new Model<T, TResult>(func, y, x, formatProvider);
 
         public static Model<T1, T2, TResult> Create<T1, T2, TResult>(Expression<Func<T1, T2, TResult>> func,
-            IReadOnlyList<TResult> y, IReadOnlyList<T1> x1, IReadOnlyList<T2> x2)
-            where T1 : IConvertible
-            where T2 : IConvertible
-            where TResult : IConvertible
-            => new Model<T1, T2, TResult>(func, y, x1, x2);
+            IReadOnlyList<TResult> y, IReadOnlyList<T1> x1, IReadOnlyList<T2> x2, IFormatProvider formatProvider = null)
+            where T1 : IComparable, IFormattable, IConvertible, IComparable<T1>, IEquatable<T1>
+            where T2 : IComparable, IFormattable, IConvertible, IComparable<T2>, IEquatable<T2>
+            where TResult : IComparable, IFormattable, IConvertible, IComparable<TResult>, IEquatable<TResult>
+            => new Model<T1, T2, TResult>(func, y, x1, x2, formatProvider);
 
         public static Model<T1, T2, T3, TResult> Create<T1, T2, T3, TResult>(Expression<Func<T1, T2, T3, TResult>> func,
-            IReadOnlyList<TResult> y, IReadOnlyList<T1> x1, IReadOnlyList<T2> x2, IReadOnlyList<T3> x3)
-            where T1 : IConvertible
-            where T2 : IConvertible
-            where T3 : IConvertible
-            where TResult : IConvertible
-            => new Model<T1, T2, T3, TResult>(func, y, x1, x2, x3);
+            IReadOnlyList<TResult> y, IReadOnlyList<T1> x1, IReadOnlyList<T2> x2, IReadOnlyList<T3> x3,
+            IFormatProvider formatProvider = null)
+            where T1 : IComparable, IFormattable, IConvertible, IComparable<T1>, IEquatable<T1>
+            where T2 : IComparable, IFormattable, IConvertible, IComparable<T2>, IEquatable<T2>
+            where T3 : IComparable, IFormattable, IConvertible, IComparable<T3>, IEquatable<T3>
+            where TResult : IComparable, IFormattable, IConvertible, IComparable<TResult>, IEquatable<TResult>
+            => new Model<T1, T2, T3, TResult>(func, y, x1, x2, x3, formatProvider);
 
         public static Model<T1, T2, T3, T4, TResult> Create<T1, T2, T3, T4, TResult>(
             Expression<Func<T1, T2, T3, T4, TResult>> func,
             IReadOnlyList<TResult> y, IReadOnlyList<T1> x1, IReadOnlyList<T2> x2, IReadOnlyList<T3> x3,
-            IReadOnlyList<T4> x4)
-            where T1 : IConvertible
-            where T2 : IConvertible
-            where T3 : IConvertible
-            where T4 : IConvertible
-            where TResult : IConvertible
-            => new Model<T1, T2, T3, T4, TResult>(func, y, x1, x2, x3, x4);
+            IReadOnlyList<T4> x4, IFormatProvider formatProvider = null)
+            where T1 : IComparable, IFormattable, IConvertible, IComparable<T1>, IEquatable<T1>
+            where T2 : IComparable, IFormattable, IConvertible, IComparable<T2>, IEquatable<T2>
+            where T3 : IComparable, IFormattable, IConvertible, IComparable<T3>, IEquatable<T3>
+            where T4 : IComparable, IFormattable, IConvertible, IComparable<T4>, IEquatable<T4>
+            where TResult : IComparable, IFormattable, IConvertible, IComparable<TResult>, IEquatable<TResult>
+            => new Model<T1, T2, T3, T4, TResult>(func, y, x1, x2, x3, x4, formatProvider);
     }
 
 
--- RegressionModel/ModelBase.cs
+++ RegressionModel/ModelBase.cs
@@ -4,6 +4,7 @@ using System.Data.SqlTypes;
 using System.Globalization;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Net.Http;
 using Markovcd.Classes;
 
 namespace Markovcd.Classes
@@ -11,29 +12,34 @@ namespace Markovcd.Classes
     public class ModelBase
     {
        
-        private static double ResidualSumOfSquares(Delegate func, IEnumerable<double> y, params IReadOnlyList<double>[][] x)
-            => y.Select((t, i) => Math.Pow(t - (double)func.DynamicInvoke(x.Select(p => p[i]).ToArray()), 2)).Sum();
+        private static double ResidualSumOfSquares(Delegate func, IEnumerable<double> y, params IReadOnlyList<double>[] x)
+        {
+            var types = func.Method.GetParameters().Skip(1).Select(p => p.ParameterType);
+            return
+                y.Select((t, i) => Math.Pow(t - CalculateFunction<double>(func, x.Select(p => (object)p[i]), types), 2))
+                    .Sum();
+        }
 
-        private static double TotalSumOfSquares<T>(IEnumerable<T> y, IFormatProvider formatProvider = null)
+        /*private static double TotalSumOfSquares<T>(IEnumerable<T> y, IFormatProvider formatProvider = null)
             where T : IConvertible
         {
             formatProvider = formatProvider ?? CultureInfo.InvariantCulture;
             return TotalSumOfSquares(y.Select(item => item.ToDouble(formatProvider)).ToList());
-        }
+        }*/
 
         private static double TotalSumOfSquares(IReadOnlyCollection<double> y)
         {
-            var mean = y.Sum() / y.Count;
+            var mean = y.Sum(item => item) / y.Count;
             return y.Sum(d => Math.Pow(d - mean, 2));
         }
 
         public static double CalculateRSquared(Delegate func, IReadOnlyList<double> y, params IReadOnlyList<double>[] x)
             => 1 - ResidualSumOfSquares(func, y, x) / TotalSumOfSquares(y);
 
-        public static LambdaExpression JoinFunction(LambdaExpression func, IEnumerable<double> coefficients)
-            => JoinFunction(SplitAndCheckLambda(func), coefficients);
+        /*public static LambdaExpression JoinFunction(LambdaExpression func, IEnumerable<double> coefficients)
+            => JoinFunction(SplitAndCheckLambda(func), coefficients);*/
 
-        protected static LambdaExpression JoinFunction(IEnumerable<LambdaExpression> func, IEnumerable<double> coefficients)
+        public static LambdaExpression JoinFunction(IEnumerable<LambdaExpression> func, IEnumerable<double> coefficients)
             => func.Zip(coefficients, (f, d) =>
                        Expression.Lambda(
                            Expression.Multiply(f.Body,
@@ -42,7 +48,7 @@ namespace Markovcd.Classes
                        Expression.Lambda(
                            Expression.Add(total.Body, curr.Body), total.Parameters));
 
-        public static double Calculate(Delegate func, params double[] x)
+        /*public static double Calculate(Delegate func, params double[] x)
             => (double)func.DynamicInvoke(x);
 
         private static double Calculate(IEnumerable<LambdaExpression> func, IEnumerable<double> coefficients, params double[] x)
@@ -50,7 +56,7 @@ namespace Markovcd.Classes
 
         public static double Calculate(LambdaExpression func, IEnumerable<double> coefficients, params double[] x)
             => Calculate(SplitAndCheckLambda(func), coefficients, x);
-
+*/
         protected static IEnumerable<LambdaExpression> SplitAndCheckLambda(LambdaExpression func)
         {
             yield return Expression.Lambda(Expression.Constant(1d), func.Parameters);
@@ -65,7 +71,6 @@ namespace Markovcd.Classes
             if (body == null)
             {
                 yield return Expression.Lambda(func.Body, func.Parameters);
-
             }
             else if (body.NodeType == ExpressionType.Add || body.NodeType == ExpressionType.AddChecked)
             {
@@ -90,7 +95,7 @@ namespace Markovcd.Classes
             }
         }
 
-        private static double SumOfFunction(LambdaExpression func, IReadOnlyList<double> y, params IReadOnlyList<double>[] x)
+        /*private static double SumOfFunction(LambdaExpression func, IReadOnlyList<double> y, params IReadOnlyList<double>[] x)
         {
             var sum = 0d;
             var del = func.Compile();
@@ -102,8 +107,43 @@ namespace Markovcd.Classes
             }
 
             return sum;
+        }*/
+
+        private static object ConvertParameterValue(object value, Type type)
+        {
+            if (value is IConvertible)
+                return Convert.ChangeType(value, type);
+
+            return value;
         }
 
+        private static IEnumerable<object> ConvertParameterValues(IEnumerable<object> values,
+            IEnumerable<Type> types) => values.Zip(types, ConvertParameterValue);
+
+        private static T CalculateFunction<T>(Delegate d, IEnumerable<object> arguments, IEnumerable<Type> types)
+        {
+            var args = ConvertParameterValues(arguments, types).ToArray();
+            return (T)d.DynamicInvoke(args);
+        }
+
+        private static dynamic SumOfFunction(LambdaExpression func, params IReadOnlyList<object>[] parameters)
+        {
+            if (!func.ReturnType.IsValueType) throw new InvalidOperationException("Function return type must be a value type.");
+        
+            var del = func.Compile();
+            var sum = 0d;
+
+            for (var i = 0; i < parameters[0].Count; i++)
+            {
+                var args = parameters.Select(arr => arr[i]);
+                var types = func.Parameters.Select(p => p.Type);
+                //sum += (double)del.DynamicInvoke(args);
+                sum += CalculateFunction<double>(del, args, types);
+            }
+
+            return sum;
+        }
+        /*
         private static double SumOfFunction(LambdaExpression func, params IReadOnlyList<double>[] x)
         {
             var sum = 0d;
@@ -117,11 +157,11 @@ namespace Markovcd.Classes
 
             return sum;
         }
+        */
+        /*public static IReadOnlyList<double> GetCoefficients(LambdaExpression func, IReadOnlyList<object> y, params IReadOnlyList<object>[] x)
+            => GetCoefficients(SplitAndCheckLambda(func).ToList(), y, x);*/
 
-        public static IReadOnlyList<double> GetCoefficients(LambdaExpression func, IReadOnlyList<double> y, params IReadOnlyList<double>[] x)
-            => GetCoefficients(SplitAndCheckLambda(func).ToList(), y, x);
-
-        protected static IReadOnlyList<double> GetCoefficients(IReadOnlyList<LambdaExpression> func, IReadOnlyList<double> y, params IReadOnlyList<double>[] x)
+        /*protected static IReadOnlyList<double> GetCoefficients(IReadOnlyList<LambdaExpression> func, IReadOnlyList<double> y, params IReadOnlyList<double>[] x)
         {
             var c = new Matrix(func.Count);
             var a = new Matrix(func.Count, func.Count);
@@ -135,26 +175,34 @@ namespace Markovcd.Classes
             }
 
             return (a.Invert() * c).GetColumn();
-        }
+        }*/
 
-        protected static IReadOnlyList<double> GetCoefficients<T, TResult>(IReadOnlyList<LambdaExpression> func, IEnumerable<TResult> y, IEnumerable<IReadOnlyList<T>> x, IFormatProvider formatProvider = null)
-            where T : IConvertible
-            where TResult : IConvertible
+        protected static IReadOnlyList<double> GetCoefficients(IReadOnlyList<LambdaExpression> func, IReadOnlyList<double> y, params IReadOnlyList<double>[] x)
         {
-            formatProvider = formatProvider ?? CultureInfo.InvariantCulture;
-            var x2 = x.Select(arr => arr.Select(item => item.ToDouble(formatProvider)).ToList()).ToArray();
-            var y2 = y.Select(item => item.ToDouble(formatProvider)).ToList();
-            return GetCoefficients(func, y2, x2);
+            var c = new Matrix(func.Count);
+            var a = new Matrix(func.Count, func.Count);
+            var yType = y.GetType().GenericTypeArguments.First();
+
+            for (var i = 0; i < a.Height; i++)
+            {
+                var concat = x.Concat(new[] {y}).Select(arr => arr.Cast<object>().ToList()).ToArray();
+                c[i] = SumOfFunction(MultiplyLambdaByY(func[i], yType), concat);
+
+                for (var j = 0; j < a.Width; j++)
+                    a[i, j] = SumOfFunction(MultiplyLambdas(func[i], func[j]), x.Select(arr => arr.Cast<object>().ToList()).ToArray());
+            }
+
+            return (a.Invert() * c).GetColumn();
         }
 
         private static LambdaExpression MultiplyLambdas(LambdaExpression func1, LambdaExpression func2)
             => Expression.Lambda(Expression.Multiply(func1.Body, func2.Body), func1.Parameters);
 
-        private static LambdaExpression MultiplyLambdaByY(LambdaExpression func)
+        private static LambdaExpression MultiplyLambdaByY(LambdaExpression func, Type type)
         {
-            var y = Expression.Parameter(typeof(double), "y");
+            var y = Expression.Parameter(type, "y");
             var body = Expression.Multiply(func.Body, y);
-            return Expression.Lambda(body, new[] {y}.Concat(func.Parameters));
+            return Expression.Lambda(body, func.Parameters.Concat(new[] {y}));
         }
     }
 }
